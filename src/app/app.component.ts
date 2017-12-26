ce0a98facde2379dbedb2cf75aaa6a69275480a3
Nacho Torrella <nachotorrella@gmail.com>
2017-12-26 10:14:03 +0100

    create input form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Angular Crud';
+  employees = [
+    { 'name': 'Nacho', position: 'manager'},
+    { 'name': 'Rauqel', position: 'CTO'},
+    { 'name': 'Alex', position: 'designer'},
+    { 'name': 'Nacho', position: 'programmer'}
+  ];
+
+  model: any = {};
+
+
+  addEmployee():void {
+
+  }
+
+  deleteEmployee():void {
+
+  }
+
+  editEmployee():void {
+
+  }
+
+  updateEmployee():void {
+
+  }
 }
