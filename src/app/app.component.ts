5e2f3f72d709dd4a4674b691a55ef919d64b913f
Nacho Torrella <nachotorrella@gmail.com>
2017-12-26 23:41:50 +0100

    final version before refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,28 +8,49 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'Angular Crud';
   employees = [
-    { 'name': 'Nacho', position: 'manager'},
-    { 'name': 'Rauqel', position: 'CTO'},
-    { 'name': 'Alex', position: 'designer'},
-    { 'name': 'Nacho', position: 'programmer'}
+    { name: 'Nacho', position: 'manager', email: 'nacho@gmail.com'},
+    { name: 'Raquel', position: 'CTO', email: 'raquel@gmail.com'},
+    { name: 'Alex', position: 'designer', email: 'alex@gmail.com'},
+    { name: 'Irene', position: 'programmer', email: 'irene@gmail.com'}
   ];
 
   model: any = {};
+  updateModel: any = {};
+  index: number = 0;
+  msg: string = '';
+  hideUpdateForm: boolean = true;
 
-
-  addEmployee():void {
-
+  addEmployee(): void {
+    this.employees.push(this.model);
+    this.msg = 'Employee added properly'
   }
 
-  deleteEmployee():void {
-
+  deleteEmployee(i): void {
+    this.employees.splice(i, 1);
+    this.msg = 'Employee removed properly';
   }
 
-  editEmployee():void {
-
+  editEmployee(i) {
+    this.updateModel.name = this.employees[i].name;
+    this.updateModel.position = this.employees[i].position;
+    this.updateModel.email = this.employees[i].email;
+    this.index = i;
+    this.hideUpdateForm = false;
   }
 
-  updateEmployee():void {
+  updateEmployee() {
+    const i = this.index;
+    for (let j = 0; j < this.employees.length; j++) {
+      if (i === j) {
+        this.employees[i] = this.updateModel;
+        this.updateEmployee = {};
+      }
+    }
+    this.msg = 'Employee edited properly';
+    this.hideUpdateForm = true;
+  }
 
+  closeAlert(): void {
+    this.msg = '';
   }
 }
